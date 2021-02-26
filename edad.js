dc973a77b4460431fb741a38435a6e96980bfe86
Santiago Bernal <exactlimon@gmail.com>
2021-02-25 20:33:33 -0500

    Calculate age
    Calculate age by year of birth

---
+++ edad.js
@@ -0,0 +1,5 @@
+alert("hola, voy a calcular tu edad a partir de tu año de nacimiento");
+let anho_nacimiento = prompt("en qué año naciste?");
+let anho_actual = 2021;
+let edad = anho_actual - anho_nacimiento;
+alert("Tu tienes " + edad + " años");
