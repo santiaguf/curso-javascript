1faa514eec232f8cac092b1a5dd24815bca50564
Santiago Bernal <exactlimon@gmail.com>
2021-02-25 19:40:06 -0500

    External JS
    Added external JS

---
+++ codigo.js
@@ -0,0 +1,5 @@
+alert("hola, voy a calcular cuánto tiempo te falta para pensionarte en Colombia");
+let edad_pension = 62;
+let edad_persona = prompt("¿cuál es tu edad?");
+let tiempo_pension = edad_pension - edad_persona;
+alert("A usted le faltan " + tiempo_pension + " años para pensionarse");
