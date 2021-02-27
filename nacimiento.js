7149a4d51c0ac4cfd506c7775abd4ba7eaebacc4
Santiago Bernal <exactlimon@gmail.com>
2021-02-26 19:25:26 -0500

    Birth Exercise

---
+++ nacimiento.js
@@ -0,0 +1,5 @@
+alert("hola, voy a calcular tu año de nacimiento a partir de tu edad");
+let edad = prompt("qué edad tienes?");
+let anho_actual = 2021;
+let anho_nacimiento = anho_actual - edad;
+alert("naciste en el año " + anho_nacimiento);
