cc09f5471673b000fb02922deeab4ce912d199d6
Santiago Bernal <exactlimon@gmail.com>
2021-03-26 20:14:30 -0500

    refactor 3

--- perros/dogs.js
+++ perros/dogs.js
@@ -6,7 +6,9 @@ function callApi(){
     redirect: 'follow'
     };
 
-    fetch("https://api.thedogapi.com/v1/images/search", requestOptions)
+    let apiUrl = "https://api.thedogapi.com/v1/images/search";
+
+    fetch(apiUrl, requestOptions)
         .then(response => response.json())
         .then(result => printData(result))
         .catch(error => console.log('error', error));
@@ -14,15 +16,17 @@ function callApi(){
 
 function printData(result){
 
+    const API_FAIL = "La API no trajo información";
+
     const image = document.querySelector('#dog-photo');
 
     image.setAttribute('src', `${result[0].url}`);
 
     const title = document.querySelector('#dog-title');
 
-    result[0].breeds[0] === undefined ? title.textContent = "la API no trajo la raza del perro" : title.textContent = `${result[0].breeds[0].name}`;
+    result[0].breeds[0] === undefined ? title.textContent = API_FAIL : title.textContent = `${result[0].breeds[0].name}`;
 
     const temperament = document.querySelector('#dog-temperament');
 
-    result[0].breeds[0] === undefined ? temperament.textContent = "la API no trajo los temperamentos del perro" : temperament.textContent = `${result[0].breeds[0].temperament}`;
+    result[0].breeds[0] === undefined ? temperament.textContent = API_FAIL : temperament.textContent = `${result[0].breeds[0].temperament}`;
 }
